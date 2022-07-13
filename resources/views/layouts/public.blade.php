29f98e6514e4bc5b22105b6675a07d6e92187a42
caleworks <mhendyjb10@gmail.com>
2022-07-13 16:02:49 +0700

    hide tulisan kodel group di layar hape

--- resources/views/layouts/public.blade.php
+++ resources/views/layouts/public.blade.php
@@ -41,8 +41,8 @@
         <div class="container">
             <header class="d-flex flex-wrap align-items-center justify-content-center justify-content-md-between py-3 mb-4 border-bottom">
               <a href="{{ url('/') }}" class="d-flex align-items-center col-md-3 mb-2 mb-md-0 text-dark text-decoration-none">
-                <img class="bi me-2" height="32" role="img" src="{{ url('assets/img/KODEL-Only_2021.png') }}" alt="">
-                <h5>Kodel Group</h5>
+                <img class="bi me-2 mt-2" height="37.6" role="img" src="{{ url('assets/img/KODEL-Only_2021.png') }}" alt="">
+                <h5 class="d-none d-xl-block mt-3">Kodel Group</h5>
               </a>
               <div class="text-end">
                 @guest
